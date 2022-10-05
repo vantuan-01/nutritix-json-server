76a549fac56ad047e01d36aa515734fa2140ba65
Pham Anh Tuan <phamanhtuan01789@gmail.com>
2022-10-05 10:06:36 +0700

    second commit

--- main.js
+++ main.js
@@ -2,10 +2,6 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 
 const routerDb = jsonServer.router("db.json");
-const routerUsers = jsonServer.router("users.json");
-const routerBills = jsonServer.router("bill.json");
-const routerTrash = jsonServer.router("trash.json");
-const routerSale = jsonServer.router("sales.json");
 
 const middlewares = jsonServer.defaults();
 const queryString = require("query-string");
@@ -32,99 +28,7 @@ server.use((req, res, next) => {
   next();
 });
 
-routerAllProduct.render = (req, res) => {
-  // check GET with pagination
-  // if yes, custom output
-
-  const headers = res.getHeaders();
-  const totalCountHeader = headers["x-total-count"];
-  console.log("header :", totalCountHeader);
-  if (req.method === "GET" && totalCountHeader) {
-    const queryParam = queryString.parse(req._parsedUrl.query);
-    console.log("query : ", queryParam);
-    const result = {
-      data: res.locals.data,
-      pagination: {
-        _page: Number.parseInt(queryParam._page) || 1,
-        _limit: Number.parseInt(queryParam._limit) || 10,
-        _totalRows: Number.parseInt(totalCountHeader),
-      },
-    };
-    return res.jsonp(result);
-  }
-  res.jsonp(res.locals.data);
-};
-
-routerUsers.render = (req, res) => {
-  // check GET with pagination
-  // if yes, custom output
-
-  const headers = res.getHeaders();
-  const totalCountHeader = headers["x-total-count"];
-  console.log("header :", totalCountHeader);
-  if (req.method === "GET" && totalCountHeader) {
-    const queryParam = queryString.parse(req._parsedUrl.query);
-    console.log("query : ", queryParam);
-    const result = {
-      data: res.locals.data,
-      pagination: {
-        _page: Number.parseInt(queryParam._page) || 1,
-        _limit: Number.parseInt(queryParam._limit) || 10,
-        _totalRows: Number.parseInt(totalCountHeader),
-      },
-    };
-    return res.jsonp(result);
-  }
-  res.jsonp(res.locals.data);
-};
-
-routerBills.render = (req, res) => {
-  // check GET with pagination
-  // if yes, custom output
-
-  const headers = res.getHeaders();
-  const totalCountHeader = headers["x-total-count"];
-  console.log("header :", totalCountHeader);
-  if (req.method === "GET" && totalCountHeader) {
-    const queryParam = queryString.parse(req._parsedUrl.query);
-    console.log("query : ", queryParam);
-    const result = {
-      data: res.locals.data,
-      pagination: {
-        _page: Number.parseInt(queryParam._page) || 1,
-        _limit: Number.parseInt(queryParam._limit) || 10,
-        _totalRows: Number.parseInt(totalCountHeader),
-      },
-    };
-    return res.jsonp(result);
-  }
-  res.jsonp(res.locals.data);
-};
-
-routerTrash.render = (req, res) => {
-  // check GET with pagination
-  // if yes, custom output
-
-  const headers = res.getHeaders();
-  const totalCountHeader = headers["x-total-count"];
-  console.log("header :", totalCountHeader);
-  if (req.method === "GET" && totalCountHeader) {
-    const queryParam = queryString.parse(req._parsedUrl.query);
-    console.log("query : ", queryParam);
-    const result = {
-      data: res.locals.data,
-      pagination: {
-        _page: Number.parseInt(queryParam._page) || 1,
-        _limit: Number.parseInt(queryParam._limit) || 10,
-        _totalRows: Number.parseInt(totalCountHeader),
-      },
-    };
-    return res.jsonp(result);
-  }
-  res.jsonp(res.locals.data);
-};
-
-routerSale.render = (req, res) => {
+routerDb.render = (req, res) => {
   // check GET with pagination
   // if yes, custom output
 
@@ -150,10 +54,6 @@ routerSale.render = (req, res) => {
 // Start server
 
 server.use("/api/product", routerDb);
-server.use("/api/user", routerUsers);
-server.use("/api/bill", routerBills);
-server.use("/api/trash", routerTrash);
-server.use("/api/sales", routerSale);
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
