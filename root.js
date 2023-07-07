f1cc2328e0cba8727cbf5f8cf6c4da4481a5f53c
Pham Anh Tuan <phamanhtuan01789@gmail.com>
2023-07-07 11:56:31 +0700

    update data

---
+++ root.js
@@ -0,0 +1,28 @@
+{
+      "id": 1,
+      "brand": "",
+      "category": "whey protein",
+      "name": "",
+      "price": 39.99,
+      "sale": 31.99,
+      "flavor": [
+        {
+          "imageId": 1,
+          "flavorName": "",
+          "imageUrl": ""
+        },
+        {
+          "imageId": 2,
+          "flavorName": "",
+          "imageUrl": ""
+        },
+        {
+          "imageId": 3,
+          "flavorName": "",
+          "imageUrl": ""
+        }
+      ],
+      "description": "",
+      "ingredients": [],
+      "subImages": []
+    },
